41113d3bd3258330cb4255cc41dd9f6fc7767a9a
Itachi <81355163+ItachiUchihaLX@users.noreply.github.com>
2021-05-15 11:12:24 +0200

    Update colorize.py

--- colorize.py
+++ colorize.py
@@ -1,3 +1,6 @@
+# Author: ItachiUchihaLX
+# Version v0.1
+
 try:    
     colors = [['31', 'red'], ['32', 'green'], ['33', 'yellow'], ['34', 'blue'], ['35', 'magenta'], ['36', 'cyan'], ['37', 'white'], ['90', 'b_black'],
               ['91', 'b_red'], ['92', 'b_green'], ['93', 'b_yellow'], ['94', 'b_blue'], ['95', 'b_magenta'], ['96', 'b_cyan'], ['97', 'b_white'], ['0', 'normal']]
@@ -13,6 +16,6 @@ try:
             print(colors[i][0], end='')            
             if i == (len(colors) - 1):
                 print(' ', end='')                     
-            print(' - ' + colorize(colors[i][1], colors[i][1]))
+            print(' - ' + color(colors[i][1], colors[i][1]))
 except:
     pass
