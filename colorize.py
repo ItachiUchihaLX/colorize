b83f887796465bdfe2e66be2982af8056148ced5
Itachi <81355163+ItachiUchihaLX@users.noreply.github.com>
2021-11-26 21:39:04 +0100

    Update colorize.py

--- colorize.py
+++ colorize.py
@@ -1,21 +1,10 @@
 # Author: ItachiUchihaLX
 # Version v0.1
 
-try:    
-    colors = [['31', 'red'], ['32', 'green'], ['33', 'yellow'], ['34', 'blue'], ['35', 'magenta'], ['36', 'cyan'], ['37', 'white'], ['90', 'b_black'],
-              ['91', 'b_red'], ['92', 'b_green'], ['93', 'b_yellow'], ['94', 'b_blue'], ['95', 'b_magenta'], ['96', 'b_cyan'], ['97', 'b_white'], ['0', 'normal']]
 
-    def colorize(text, col):
-        for i in range(len(colors)):
-            if col == colors[i][1]:
-                return str('\033[1;' + colors[i][0] + 'm' + text + '\033[m')
-        return None
+colors = [['31', 'red'], ['32', 'green'], ['33', 'yellow'], ['34', 'blue'], ['35', 'magenta'], ['36', 'cyan'], ['37', 'white'], ['90', 'b_black'],
+            ['91', 'b_red'], ['92', 'b_green'], ['93', 'b_yellow'], ['94', 'b_blue'], ['95', 'b_magenta'], ['96', 'b_cyan'], ['97', 'b_white'], ['0', 'standart']]
 
-    def help():
-        for i in range(len(colors)):
-            print(colors[i][0], end='')            
-            if i == (len(colors) - 1):
-                print(' ', end='')                     
-            print(' - ' + color(colors[i][1], colors[i][1]))
-except:
-    pass
+for color in colors:
+    exec(f'def {color[1]}(text, toggle=False):\n toggle = "" if toggle else "\033[m" \n return f"\033[1;{color[0]}m" + text + toggle')
+
